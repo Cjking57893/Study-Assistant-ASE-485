3ef092bf61ab243fd61ca14bca74b627989bbc3f
Cody King <cody.king04@outlook.com>
2026-04-24 22:25:25 -0400

    Fix Express 5 catch-all route syntax

--- server/src/app.ts
+++ server/src/app.ts
@@ -36,7 +36,7 @@ app.use('/api', authenticateToken, generateRoutes);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, '../../dist')));
-app.get('*', (_req, res) => {
+app.get('{*path}', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
